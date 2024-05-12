4adb92b0bad04d4d8c4097ecb00f93f7c835a7cf
shmzzzz <slt0623@gmail.com>
2024-05-12 10:51:19 +0900

    test: 初期表示時のUI Testケースの追加

---
+++ app/src/androidTest/java/com/example/tipcalcapp/MainActivityTest.kt
@@ -0,0 +1,27 @@
+package com.example.tipcalcapp
+
+import androidx.compose.ui.test.click
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTouchInput
+import org.junit.Rule
+import org.junit.Test
+
+class MainActivityTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule(MainActivity::class.java)
+
+    @Test
+    fun test_TopHeaderの表示確認() {
+        composeTestRule.onNodeWithText("Total Per Person").assertExists()
+        composeTestRule.onNodeWithText("$0.00").assertExists()
+    }
+
+    @Test
+    fun test_BillFormの表示確認_初期表示() {
+        composeTestRule.onNodeWithText("Enter Bill").assertExists()
+        composeTestRule.onNodeWithText("Enter Bill").performTouchInput {
+            click()
+        }
+    }
+}
